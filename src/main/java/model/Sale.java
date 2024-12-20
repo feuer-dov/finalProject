bef001a0332c722c1843e81fa37b6f65b61a4074
Sean Malins-Umansky <smu@my.yorku.ca>
2024-12-19 20:13:24 -0500

    Changed Sale Class to add CCnum and Address

--- src/main/java/model/Sale.java
+++ src/main/java/model/Sale.java
@@ -9,15 +9,34 @@ public class Sale implements Serializable{
 	private ArrayList<Integer> quantity;
 	private int transactionId;
 	private double total;
+	private String address;
+	private String ccNumber;
 	
-	public Sale(String cname, double total, ArrayList<Integer> id, ArrayList<Integer> qty, int tId) {
+	public Sale(String cname, double total, ArrayList<Integer> id, ArrayList<Integer> qty, int tId, String shippingAddress, String ccNumber) {
 		this.custName = cname;
 		this.itemId = id;
 		this.quantity = qty;
 		this.transactionId = tId;
 		this.total = total;
+		this.address = shippingAddress;
+		this.ccNumber = ccNumber;
 	}
 	
+	public String getCCNumber() {
+		return this.getCCNumber();
+	}
+	
+	public void setCCNumber(String newCC) {
+		this.ccNumber = newCC;
+	}
+	
+	public String getShippingAddress() {
+		return this.address;
+	}
+	
+	public void setShippingAddress(String newAddress) {
+		this.address = newAddress;
+	}
 	
 	public String getCustName() {
 		return this.custName;
