bc98cc8ca6c51114acffcf931507b127d4884e4d
Dov Feuer <dovfeuer@gmail.com>
2024-12-19 21:52:32 -0500

    account information page created

--- src/main/java/controller/AccountInfo.java
+++ src/main/java/controller/AccountInfo.java
@@ -8,6 +8,9 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import model.Account;
 
 /**
  * Servlet implementation class AccountInfo
@@ -28,7 +31,27 @@ public class AccountInfo extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		String action = request.getParameter("action");
+		DatabaseInterface db = new Database(request.getServletContext());
+		HttpSession session = request.getSession();
+		
+		if(action.equals("updateInfo")) {
+			String username = request.getParameter("username");
+			String name = request.getParameter("name");
+			String billingAddress = request.getParameter("billingAddress");
+			String shipAddress = request.getParameter("shipAddress");
+			String creditCard = request.getParameter("creditCard");
+			
+			Account a1 = db.getAccount(username);
+			Account account = new Account(shipAddress, username, a1.getPassword(), creditCard, name, 1, billingAddress);
+			db.updateAccount(username, account);
+			session.setAttribute("account", account);
+		}
+		else if(action.equals("logOut")) {
+			session.setAttribute("account", null);
+			request.getRequestDispatcher("home").forward(request, response);;
+		}
+		
 		RequestDispatcher rd = request.getRequestDispatcher("jsp/accountInfo.jsp");
 		rd.forward(request, response);
 	}
